c09d59579459c570415da4c7eae686cd0721da65
Egor Soluynov <167007583+TechStrelka@users.noreply.github.com>
2026-05-12 19:34:46 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,15 +1,74 @@
-// Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+ private:
+    struct Item {
+        T data;
+        Item* next;
+    };
+    Item *head, *tail;
+    Item* create(const T& data) {
+        Item* item = new Item;
+        item->data = data;
+        item->next = nullptr;
+      
+        return item;
+    }
+
+ public:
+    TPQueue() : head(nullptr), tail(nullptr) {}
+
+    ~TPQueue() {
+        while (head) {
+            pop();
+        }
+    }
+
+    void push(const T& data) {
+        Item* newItem = create(data);
+
+        if (head == nullptr) {
+            head = tail = newItem;
+        }
+        else if (data.prior > head->data.prior) {
+            newItem->next = head;
+            head = newItem;
+        }
+        else {
+            Item* current = head;
+            while (current->next != nullptr && current->next->data.prior >= data.prior) {
+                current = current->next;
+            }
+            newItem->next = current->next;
+            current->next = newItem;
+            if (newItem->next == nullptr) {
+                tail = newItem;
+            }
+        }
+    }
+
+    T pop() {
+        if (head == nullptr) {
+            return T(); 
+        }
+
+        Item* temp = head;
+        T data = temp->data;
+
+        head = head->next;
+        if (head == nullptr) {
+            tail = nullptr;
+        }
+        delete temp;
+        return data;
+    }
 };
 
 struct SYM {
-  char ch;
-  int prior;
+    char ch;
+    int prior;
 };
 
 #endif  // INCLUDE_TPQUEUE_H_
